5aa4478fee6d86b867a237c5baf8c1db9a18028d
Doqso <p3lopubico@gmail.com>
2022-11-07 17:47:58 +0100

    commit 4

--- app/src/main/java/com/giorgigongadze/practica1erexamen/BienvenidaActivity.kt
+++ app/src/main/java/com/giorgigongadze/practica1erexamen/BienvenidaActivity.kt
@@ -2,10 +2,13 @@ package com.giorgigongadze.practica1erexamen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class BienvenidaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
+        var tvBienvenido = findViewById<TextView>(R.id.tvBienvenido)
+        tvBienvenido.setText(intent.getStringExtra("nombre"))
     }
 }
--- app/src/main/java/com/giorgigongadze/practica1erexamen/MainActivity.kt
+++ app/src/main/java/com/giorgigongadze/practica1erexamen/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.giorgigongadze.practica1erexamen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,12 @@ class MainActivity : AppCompatActivity() {
     fun btVerificar_onClick(view: View) {
         var etNombre = findViewById<EditText>(R.id.etNombre).text
         var etClave = findViewById<EditText>(R.id.etClave).text
+        if (!etClave.toString().equals("abcdef*")) {
+            Toast.makeText(this, "Clave errónea!!!", Toast.LENGTH_LONG)
+            return
+        }
+        var intent = Intent(this, BienvenidaActivity::class.java)
+        intent.putExtra("nombre", etNombre.toString())
+        startActivity(intent)
     }
 }
