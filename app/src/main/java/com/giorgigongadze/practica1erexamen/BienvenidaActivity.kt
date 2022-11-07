40f4bc25d0e9e387704c61cdf82c3f6d90f2a3c5
Doqso <p3lopubico@gmail.com>
2022-11-07 17:40:17 +0100

    commit 3

---
+++ app/src/main/java/com/giorgigongadze/practica1erexamen/BienvenidaActivity.kt
@@ -0,0 +1,11 @@
+package com.giorgigongadze.practica1erexamen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BienvenidaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenida)
+    }
+}
--- app/src/main/java/com/giorgigongadze/practica1erexamen/MainActivity.kt
+++ app/src/main/java/com/giorgigongadze/practica1erexamen/MainActivity.kt
@@ -2,10 +2,17 @@ package com.giorgigongadze.practica1erexamen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btVerificar_onClick(view: View) {
+        var etNombre = findViewById<EditText>(R.id.etNombre).text
+        var etClave = findViewById<EditText>(R.id.etClave).text
     }
 }
